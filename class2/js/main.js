7bdaa6e602539cf69b5e617e8ae705b55122a436
CavadAhmedov <36768138+CavadAhmedov@users.noreply.github.com>
2018-04-10 21:35:17 +0400

    Add files via upload

---
+++ class2/js/main.js
@@ -0,0 +1,25 @@
+var third = document.querySelector(".lvl3");
+
+document.addEventListener("keyup", function(e){
+	if(e.keyCode === 13){
+		third.style.display = "block";
+		
+	}
+})
+
+
+
+// ext.addEventListener("keypress", function(e){
+// 		if(e.keyCode === 13){
+// 			submitButton1.click;
+// 		}
+
+// setInterval(function){
+// 	var main document.querySelector("main");
+// 	var date = new Date();
+// 	main.innerText = date.getFullYear()+ "/" +date.getDate() +"/"+ date.getHours()
+// }
+// var all = document.query.querySelectorAll(".l1 , .l2 , .l3");
+// for(var item of all){
+// 	item.addEventListener("click",function(e))
+// }
